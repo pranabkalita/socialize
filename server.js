24836a56780cf4bd9f885b7ee3d00698420bc78b
Pranab Kalita <pavankalita@gmail.com>
2021-06-21 20:51:23 +0530

    Created Post Schema and Model

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const { Schema } = mongoose
 
 mongoose
   .connect('mongodb://localhost:27017/socialize', {
@@ -12,6 +13,23 @@ mongoose
   .catch((error) => {
     console.log(`Error connecting database: ${error}`)
   })
+
+const postSchema = new Schema(
+  {
+    title: String,
+    slug: String,
+    body: String,
+    coverImage: String,
+    images: [String],
+    isDraft: { type: Boolean, default: false },
+    isPublished: { type: Boolean, default: true },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Post = mongoose.model('Post', postSchema)
 
 const app = express()
 
